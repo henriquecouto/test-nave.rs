89b7e8a1d20ca610721b1a1ee691271fd5474101
Henrique Couto <henrique.coutu@live.com>
2020-08-25 00:28:08 -0300

    add react-navigation

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar, SafeAreaView, ScrollView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <Text>Hello Nave Test</Text>
         </ScrollView>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
