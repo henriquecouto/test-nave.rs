994695d73162418ce0afc52608db2298e37433e9
Henrique Couto <henrique.coutu@live.com>
2020-08-25 22:32:52 -0300

    create theme

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+
 import Router from './src/router';
+import defaultTheme from './src/styles/themes/default';
 
 const App = () => {
   return (
-    <>
-      <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+    <ThemeProvider theme={defaultTheme}>
+      <StatusBar
+        backgroundColor={defaultTheme.palette.background}
+        barStyle="dark-content"
+      />
       <Router />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/themes/default.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'default',
+  palette: {
+    primary: '#212121',
+    secondary: '#FFFFFF',
+    placeholder: '#9E9E9E',
+    background: '#FAFAFA',
+  },
+};
