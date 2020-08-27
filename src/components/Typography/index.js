af8b61717068b8e8c137891548eb9ff014588509
Henrique Couto <henrique.coutu@live.com>
2020-08-27 18:58:11 -0300

    create Typography component

---
+++ src/components/Typography/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const H1 = styled.Text`
+  font-family: 'Montserrat-SemiBold';
+  font-size: 22px;
+  line-height: 32px;
+`;
+
+const H2 = styled.Text`
+  font-family: 'Montserrat-Regular';
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export default {H1, H2};
