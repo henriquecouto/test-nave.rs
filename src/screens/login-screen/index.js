c5dc75c89880a77eb429d16891568975dd217594
Henrique Couto <henrique.coutu@live.com>
2020-08-27 19:03:27 -0300

    finish login screen

--- src/screens/login-screen/index.js
+++ src/screens/login-screen/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {CommonActions} from '@react-navigation/native';
 
 import {Container, Logo, Form} from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import makeLogin from '../../helpers/actions/auth';
+import Alert from '../../components/Alert';
 
-export default function LoginScreen() {
+export default function LoginScreen({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const callMakeLoginAction = () => {
+    setError('');
+    const onSuccess = () => {
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 1,
+          routes: [{name: 'Home'}],
+        }),
+      );
+    };
+    const onError = (errorMessage) => {
+      setError(errorMessage);
+    };
+    makeLogin(email, password, onSuccess, onError);
+  };
+
   return (
     <Container>
       <Logo />
       <Form>
-        <Input label="E-mail" />
-        <Input label="Senha" />
-        <Button>Entrar</Button>
+        <Alert
+          visible={!!error}
+          title="Erro"
+          message={error}
+          onClose={() => setError('')}
+        />
+        <Input
+          label="E-mail"
+          value={email}
+          onChangeText={setEmail}
+          autoCompleteType="email"
+        />
+        <Input
+          secureTextEntry
+          label="Senha"
+          value={password}
+          onChangeText={setPassword}
+          autoCompleteType="password"
+        />
+        <Button onPress={callMakeLoginAction}>Entrar</Button>
       </Form>
     </Container>
   );
